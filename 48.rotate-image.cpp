f5bf4d815915c221c50b5d245a6d099de0aebfc6
Efaz <efaz701815@gmail.com>
2021-05-07 09:04:19 +0600

    matrix

--- 48.rotate-image.cpp
+++ 48.rotate-image.cpp
@@ -8,7 +8,31 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        //start
+        
+        // mirror accross main diagonal
+        int n =  matrix.size();
+        int temp;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<i;j++) {
+                // swap
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // mirror accross vertical middle axis
+    
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<(n/2);j++) {
+                //swap
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+        
+        
     }
 };
 // @lc code=end
